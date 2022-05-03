08e2b547238ec36df76c353f679a77bd40c06d3f
Nadim Searight <ns241@st-andrews.ac.uk>
2022-05-03 14:57:49 +0100

    it does not work

--- src/Driver.java
+++ src/Driver.java
@@ -1,2 +1,33 @@
+import java.util.List;
+
 public class Driver {
+    static double [][] X= {
+            {0,0},
+            {1,0},
+            {0,1},
+            {1,1}
+    };
+    static double [][] Y= {
+            {0},{1},{1},{0}
+    };
+
+    public static void main(String[] args) {
+        NeuralNetwork nn = new NeuralNetwork(2,10,1);
+        nn.fit(X, Y, 5000);
+
+        List<Double> output;
+
+
+        double[][] input = {
+                {0,0},
+                {0,1},
+                {1,0},
+                {1,1}
+        };
+
+        for(double[] d : input) {
+            output = nn.predict(d);
+            System.out.println(output.toString());
+        }
+    }
 }
